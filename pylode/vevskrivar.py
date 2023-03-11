e12b51650265d5448e38ae9c4c2a9b8ea788c38a
markus-ap <markusap@proton.me>
2023-03-11 01:22:24 +0100

    fyrste avlevering

--- pylode/vevskrivar.py
+++ pylode/vevskrivar.py
@@ -1,15 +1,19 @@
 from pylode import OntDoc
-import shutil, os
+import os
 
 def lag():
     mappe = os.getcwd()
-    os.mkdir("html")
-    for fil in os.listdir(os.getcwd()):
+    
+    os.chdir("..")
+    if not os.path.exists("doc"):
+        os.mkdir("doc") 
+
+    for fil in os.listdir(mappe):
         if fil.endswith(".ttl"):
-            filsti = f"{mappe}\{fil}"
+            filsti = f"{mappe}/{fil}"
             od = OntDoc(ontology=filsti)
             
-            html_filnamn = f"html/{fil.replace('.ttl', '.html')}"
+            html_filnamn = f"doc\{fil.replace('.ttl', '.html')}"
             od.make_html(html_filnamn)
 
 if __name__ == "__main__":
